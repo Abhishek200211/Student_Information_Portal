ec2871fa489ef1269a4ea82d89c1b93da2fb1c5d
Abhishek kumar sinha <104313529+Abhishek200211@users.noreply.github.com>
2023-01-28 23:25:10 +0530

    Add files via upload
    student Information  portal with sql connectivity

---
+++ studentinf/src/main/java/JOptionPane.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aastha Sinha
+ */
+class JOptionPane {
+
+    static void showMessageDialog(Object object, String record_Addedddd) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
